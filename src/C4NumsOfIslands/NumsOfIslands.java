07dd68e597eecffa76f4de07bd4157081dd2501b
Linqiang Guo <105519688+pockyitachi@users.noreply.github.com>
2022-05-27 22:23:23 -0400

    Update

--- src/C4NumsOfIslands/NumsOfIslands.java
+++ src/C4NumsOfIslands/NumsOfIslands.java
@@ -1,4 +1,36 @@
 package C4NumsOfIslands;
 
 public class NumsOfIslands {
+    final static int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    public int numIslands(char[][] grid) {
+        //sanity check
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid, i, j, rows, cols);
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] array, int x, int y, int rows, int cols){
+        // base case
+        if(x < 0 || x >= rows || y < 0 || y >= cols || array[x][y] == '0'){
+            return;
+        }
+        // recursion rule
+        array[x][y] = '0';
+        for(int[] dir : dirs){
+            int neighbourX = x + dir[0];
+            int neighbourY = y + dir[1];
+            dfs(array, neighbourX, neighbourY, rows, cols);
+        }
+    }
 }
--- src/C4NumsOfIslands/test.java
+++ src/C4NumsOfIslands/test.java
@@ -1,4 +1,13 @@
 package C4NumsOfIslands;
 
 public class test {
+    public static void main(String[] args) {
+        char[][] grid = {{'1','1','0','0','0'},
+                         {'1','1','0','0','0'},
+                         {'0','0','1','0','0'},
+                         {'0','0','0','1','1'}};
+        NumsOfIslands test = new NumsOfIslands();
+        int num = test.numIslands(grid);
+        System.out.println(num);
+    }
 }
